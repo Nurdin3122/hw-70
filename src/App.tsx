a6846fa3b94d94bad0e159553604c4f509480afe
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-07-22 21:47:31 +0600

    init app,установил:react-router-dom,axios,bootstrap,redux js/toolkit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+const App = () => (
+    <>
+        <p>Hello World</p>
+    </>
+);
+
+export default App
---
+++ src/Redux/store/store.ts
@@ -0,0 +1,18 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {cartReducer} from "./store/CartSlice";
+
+
+
+export const store = configureStore({
+
+    reducer: {
+
+    }
+
+});
+
+
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
